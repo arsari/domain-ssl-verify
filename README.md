e5072c2a88b9f3e00a940a3601d3d13c39a5d397
arsari <asantiago@arsari.com>
2018-04-13 21:23:36 -0400

    Improvement and stability. Menu slection.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# domain-ssl-verify
-Verify if each domain in an array have HTTP or HTTPS protocol
+# Domain-SSL-Verify
+---
+
+This script do an HTTP request of a list of domain names storage in a CSV file or SQL database and fetches all the headers sent by the domain server returning an associate array with each domain header information.
+
+When the list is on an ***SQL database***, the script update the table of the list of domains adding to the database the resulting SSL status in in an existing column named **SSLcertificate**.
--- domian-ssl-verify.php
+++ domian-ssl-verify.php
@@ -2,31 +2,88 @@
 /**
  * PHP Version 7.1.9
  *
- * This script read a csv file with a list of domains and fetches all the
- * headers sent by a domain server in response to a HTTP request from an
- * array of domains and returning an associate array with each domain
- * header information
+ * This script do an HTTP request of a list of domain names
+ * storage in a CSV file or SQL database and fetches all the
+ * headers sent by the domain server returning an associate
+ * array with each domain header information. When the list
+ * is on an SQL database, the script update the table of
+ * the list of domains adding to the database the resulting
+ * SSL status in in an existing column named SSLcertificate.
  *
  * @category HTTP_Request
  * @package  None
  * @author   Arturo Santiago <asantiago@arsari.com>
  * @license  MIT License
- * @link     https://www.arsari.githib.io
+ * @link     https://arsari.github.io
  */
-ini_set('display_errors', 0); // 0 = hide errors; 1 = display errors
 
-// read csv file content
-$rows = file('domains.csv');
-$data = array_pop($rows);
-$clients_domains = str_getcsv($data);
-$domains_arr_length = count($clients_domains);
+// disable warnings and notices (0 = hide errors; -1 = display errors)
+error_reporting(0);
 
-// global variables definition
-// $clients_domains = array("micasa.net", "myhome.net", "agency.com", "batman.com", "arsari.com", "aviation.com", "housekeeping.com");
-// $domains_arr_length = count($clients_domains);
+// global variables
+$dead = '404 Not Found';
 $ssl = 'Secure';
 $non_ssl = 'Non-Secure';
+$redirect = 'Redirect > ';
+$blocked = '403 Forbidden > ';
+$down = 'Error > Non-Existing?';
+$parked = 'Parked';
 $separator = ">\t";
+$header = str_pad(" Testing Domains ", 80, "=", STR_PAD_BOTH) . "\n";
+$footer = str_pad(" End Testing ", 80, "=", STR_PAD_BOTH) . "\n";
+
+/**
+ * Function to evaluated a domains
+ *
+ * @param Array $domains array with list of domains names
+ *
+ * @return String $ssl_status return ssl status
+ */
+function domainsEval($domains)
+{
+    $domains_arr_length = count($domains);
+
+    for ($x = 0; $x < $domains_arr_length; $x++) {
+        $url = "http://www." . $domains[$x] . "/"; // domain url to evaluate
+        $headers = get_headers($url, 1); // returns an associated array with index keys of the domain server headers
+
+        // look for the value of the index key 'Location' or 'location' in the headers request
+        if ($headers['location'] !== null) {
+            $url_eval = $headers['location'];
+        } else {
+            $url_eval = $headers['Location'];
+        }
+
+        if (strpos($headers[0], '404')) {
+            $ssl_status = $GLOBALS['dead'];
+            echo str_pad($url, 52, " .") . $GLOBALS['separator'] . $GLOBALS['dead'] . "\n";
+        } elseif (strpos($headers[0], '403')) {
+            $ssl_status = $GLOBALS['blocked'] . $GLOBALS['ssl'];
+            echo str_pad($url, 52, " .") . $GLOBALS['separator'] . $GLOBALS['blocked'] . $GLOBALS['ssl'] . "\n";
+        } elseif (is_array($url_eval)) {
+            $ssl_status = urlRedirect($url, $url_eval);
+        } elseif (strpos($url_eval, 'https') !== false) {
+            $ssl_status = $GLOBALS['ssl'];
+            echo str_pad($url_eval, 52, " .") . $GLOBALS['separator'] . $GLOBALS['ssl'] . "\n";
+        } elseif ($url_eval !== null) {
+            $ssl_status = $GLOBALS['non_ssl'];
+            echo str_pad($url_eval, 52, " .") . $GLOBALS['separator'] . $GLOBALS['non_ssl'] . "\n";
+        } elseif ($url_eval === null and ((strpos($headers[0], '200') !== false) or (strpos($headers[0], '301') !== false))) {
+            $ssl_status = $GLOBALS['non_ssl'];
+            echo str_pad($url, 52, " .") . $GLOBALS['separator'] . $GLOBALS['non_ssl'] . "\n";
+        } else {
+            $ssl_status = $GLOBALS['down'];
+            echo str_pad($url, 52, " .") . $GLOBALS['separator'] . $GLOBALS['down'] . "\n";
+        }
+
+    }
+
+    if ($ssl_status === null) {
+        $ssl_status = $GLOBALS['parked'];
+    }
+
+    return $ssl_status;
+}
 
 /**
  * Function to evaluated a domain array redirection
@@ -34,46 +91,139 @@ $separator = ">\t";
  * @param String $url      domain with redirection
  * @param Array  $url_eval array of redirected domains
  *
- * @return None
+ * @return String $ssl_status return ssl status
  */
 function urlRedirect($url, $url_eval)
 {
-    echo $url . "\t[ Domain redirect to: ]\n";
+    echo $url . "  [Domain redirect to:]\n";
 
     for ($y = 0; $y < 3; $y++) {
         $url_eval_arr = $url_eval[$y];
 
         if ($url_eval_arr === null or strpos($url_eval_arr, 'http') === false) {
+            $ssl_status = null;
             continue;
-        } elseif (strpos($url_eval_arr, 'https')) {
-            echo "\t" . str_pad($url_eval_arr, 44, " .") . $GLOBALS['separator'] . $GLOBALS['ssl'] . "\n";
+        } elseif (strpos($url_eval_arr, 'https') !== false) {
+            $ssl_status = $GLOBALS['redirect'] . $GLOBALS['ssl'];
+            echo "\t" . str_pad($url_eval_arr, 44, " .") . $GLOBALS['separator'] . $GLOBALS['redirect'] . $GLOBALS['ssl'] . "\n";
         } else {
-            echo "\t" . str_pad($url_eval_arr, 44, " .") . $GLOBALS['separator'] . $GLOBALS['non_ssl'] . "\n";
+            $ssl_status = $GLOBALS['redirect'] . $GLOBALS['non_ssl'];
+            echo "\t" . str_pad($url_eval_arr, 44, " .") . $GLOBALS['separator'] . $GLOBALS['redirect'] . $GLOBALS['non_ssl'] . "\n";
         }
     }
+    return $ssl_status;
 }
 
-// main sript
-echo str_pad(" Testing Domains ", 66, "=", STR_PAD_BOTH) . "\n";
-echo "\n";
+/**
+ * Function to read a CSV file
+ *
+ * @return None
+ */
+function readCSV()
+{
+    // User input file name
+    echo "\nEnter file name without '.csv': ";
+    $csv_file = trim(fgets(STDIN));
+    echo "\n";
 
-for ($x = 0; $x < $domains_arr_length; $x++) {
-    $url = "http://www.{$clients_domains[$x]}"; // domain url to evaluate
-    $headers = get_headers($url, 1); // returns an associated array with index keys of the domain server headers
-    $url_eval = $headers['Location']; // look for the value of the index key 'Location'
+    $file_name = $csv_file . ".csv";
 
-    if (strpos($headers[0], '404')) {
-        echo str_pad($url, 52, " .") . $separator . "Not Found", "\n";
-    } elseif (is_array($url_eval)) {
-        urlRedirect($url, $url_eval);
-    } elseif ($url_eval) {
-        echo str_pad($url_eval, 52, " .") . $separator . $ssl . "\n";
+    // Read CSV file
+    $row = 1;
+    if (($handle = fopen($file_name, "r")) !== false) { // read csv file content
+        echo $GLOBALS[header] . "\n";
+        while (($domains = fgetcsv($handle, 1000, ",")) !== false) {
+            domainsEval($domains);
+            $row++;
+        }
+        fclose($handle);
     } else {
-        echo str_pad($url, 52, " .") . $separator . $non_ssl . "\n";
+        echo "*** ERROR: Failed to connect to file. ***\n";
     }
 }
 
-echo "\n";
-echo str_pad(" End Testing ", 66, "=", STR_PAD_BOTH) . "\n";
+/**
+ * Function to read an SQL database
+ *
+ * @return None
+ */
+function readDB()
+{
+    // Sql credentials and variables input
+    echo "[Database login credentials and db information]";
+    echo "Enter [HOST] name: ";
+    $host = trim(fgets(STDIN));
+    echo "Enter [USER] name: ";
+    $user = trim(fgets(STDIN));
+    echo "Enter [PASSWORD] for user: ";
+    $password = trim(fgets(STDIN));
+    echo "Enter [DATABASE] name: ";
+    $db = trim(fgets(STDIN));
+    echo "Enter [TABLE] name: ";
+    $table = trim(fgets(STDIN));
+    echo "Enter [COLUMN] name: ";
+    $column = trim(fgets(STDIN));
+    echo "\n";
 
-// print_r(get_headers($url, 1));
+    // db testing credentials
+    // $host = "";
+    // $user = "asantiago";
+    // $password = "";
+    // $db = "Domains";
+    // $table = "Names";
+    // $column = "DomainName";
+
+    // Initiate sql connection
+    $conn = mysqli_connect($host, $user, $password, $db);
+
+    // Check connection
+    if (mysqli_connect_errno()) {
+        echo "\n*** ERROR: Failed to connect to database [" . mysqli_connect_error() . "]***\n";
+    } else {
+        // Look on table
+        $sql_query_r = "SELECT {$column} FROM {$table} ORDER BY {$table}.{$column}";
+
+        echo "\n" . $GLOBALS[header] . "\n";
+
+        if ($result = mysqli_query($conn, $sql_query_r)) {
+            // Fetch one and one row
+            while ($domains = mysqli_fetch_row($result)) {
+                $ssl_status = domainsEval($domains);
+                // insert domain ssl status into db
+                $sql_query_w = "UPDATE {$table} SET SSLcertificate = '{$ssl_status}' WHERE {$column} = '{$domains[0]}'";
+                mysqli_query($conn, $sql_query_w);
+            }
+            // Free result set
+            mysqli_free_result($result);
+        }
+    }
+    // End sql connection
+    mysqli_close($conn);
+}
+
+// Initial output
+$input = -1;
+
+while ($input != 0) {
+    echo "\nWhere is the list of domians to evaluate?\n";
+    echo "1 => CSV File\n";
+    echo "2 => SQL Database\n";
+    echo "0 => Exit\n";
+    echo "\nSelect option: ";
+
+    $input = trim(fgets(STDIN));
+
+    if ($input == '1') {
+        echo "\n<<< Testing from CSV file >>>\n";
+        readCSV();
+        echo "\n" . $footer;
+    } elseif ($input == '2') {
+        echo "\n<<< Testing from Database >>>\n";
+        readDB();
+        echo "\n" . $footer;
+    } elseif ($input == '0') {
+        exit("\n<<< Good Bye!! >>>\n\n");
+    } else {
+        echo "\n*** Error: Option Invalid ***\n";
+    }
+}
